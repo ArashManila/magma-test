48838cc1ce8dca4646d78661fbaa17b908333a3f
Igor Wilhelm <wilgelm-igor@yandex.ru>
2025-02-07 11:20:30 +0600

    начальная верстка и плейсхолдер данных

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <hr/>
+        <div className="nav-block">
+
+        </div>
+        <button className="button header-button">Создать проект</button>
+      </header>
+      <table>
+        <thead>
+            <tr>
+                <th>Столбец 1 </th>
+                <th>Столбец 2 </th>
+                <th>Столбец 3 </th>
+                <th>Столбец 4 </th>
+                <th>Столбец 5 </th>
+                <th>Столбец 6 </th>
+            </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Данные 11</td>
+            <td>Данные 12</td>
+            <td>Данные 13</td>
+            <td>Данные 14</td>
+            <td>Данные 15</td>
+            <td>Данные 16</td>
+          </tr>
+          <tr>
+            <td>Данные 21</td>
+            <td>Данные 22</td>
+            <td>Данные 23</td>
+            <td>Данные 24</td>
+            <td>Данные 25</td>
+            <td>Данные 26</td>
+          </tr>
+          <tr>
+            <td>Данные 31</td>
+            <td>Данные 32</td>
+            <td>Данные 33</td>
+            <td>Данные 34</td>
+            <td>Данные 35</td>
+            <td>Данные 36</td>
+          </tr>
+          <tr>
+            <td>Данные 41</td>
+            <td>Данные 42</td>
+            <td>Данные 43</td>
+            <td>Данные 44</td>
+            <td>Данные 45</td>
+            <td>Данные 46</td>
+          </tr>
+          <tr>
+            <td>Данные 51</td>
+            <td>Данные 52</td>
+            <td>Данные 53</td>
+            <td>Данные 54</td>
+            <td>Данные 55</td>
+            <td>Данные 56</td>
+          </tr>
+          <tr>
+            <td>Данные 61</td>
+            <td>Данные 62</td>
+            <td>Данные 63</td>
+            <td>Данные 64</td>
+            <td>Данные 65</td>
+            <td>Данные 66</td>
+          </tr>
+          <tr>
+            <td>Данные 71</td>
+            <td>Данные 72</td>
+            <td>Данные 73</td>
+            <td>Данные 74</td>
+            <td>Данные 75</td>
+            <td>Данные 76</td>
+          </tr>
+          <tr>
+            <td>Данные 81</td>
+            <td>Данные 82</td>
+            <td>Данные 83</td>
+            <td>Данные 84</td>
+            <td>Данные 85</td>
+            <td>Данные 86</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
