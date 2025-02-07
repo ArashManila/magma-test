32df3c0561c1c48c2d874eec918e08ca9e6deab5
Igor Wilhelm <wilgelm-igor@yandex.ru>
2025-02-07 12:51:22 +0600

    Добавлены header и начата верстка таблицы

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,98 @@
-
+import homeIcon from './images/home-icon.svg'
 
 function App() {
   return (
     <>
       <header className="header">
         <hr/>
-        <div className="nav-block">
-
+        <div className="header__nav-block">
+          <img src={homeIcon} className="header-icon" alt="home" width={16} height={16}/>
+          &gt;
+          <span>Проекты</span>
         </div>
         <button className="button header-button">Создать проект</button>
       </header>
-      <table>
-        <thead>
+      <main className="content">
+        <table>
+          <thead>
+              <tr>
+                  <th>Столбец 1 </th>
+                  <th>Столбец 2 </th>
+                  <th>Столбец 3 </th>
+                  <th>Столбец 4 </th>
+                  <th>Столбец 5 </th>
+                  <th>Столбец 6 </th>
+              </tr>
+          </thead>
+          <tbody>
             <tr>
-                <th>Столбец 1 </th>
-                <th>Столбец 2 </th>
-                <th>Столбец 3 </th>
-                <th>Столбец 4 </th>
-                <th>Столбец 5 </th>
-                <th>Столбец 6 </th>
+              <td><span>Данные 11</span></td>
+              <td><span>Данные 12</span></td>
+              <td><span>Данные 13</span></td>
+              <td><span>Данные 14</span></td>
+              <td><span>Данные 15</span></td>
+              <td><span>Данные 16</span></td>
             </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Данные 11</td>
-            <td>Данные 12</td>
-            <td>Данные 13</td>
-            <td>Данные 14</td>
-            <td>Данные 15</td>
-            <td>Данные 16</td>
-          </tr>
-          <tr>
-            <td>Данные 21</td>
-            <td>Данные 22</td>
-            <td>Данные 23</td>
-            <td>Данные 24</td>
-            <td>Данные 25</td>
-            <td>Данные 26</td>
-          </tr>
-          <tr>
-            <td>Данные 31</td>
-            <td>Данные 32</td>
-            <td>Данные 33</td>
-            <td>Данные 34</td>
-            <td>Данные 35</td>
-            <td>Данные 36</td>
-          </tr>
-          <tr>
-            <td>Данные 41</td>
-            <td>Данные 42</td>
-            <td>Данные 43</td>
-            <td>Данные 44</td>
-            <td>Данные 45</td>
-            <td>Данные 46</td>
-          </tr>
-          <tr>
-            <td>Данные 51</td>
-            <td>Данные 52</td>
-            <td>Данные 53</td>
-            <td>Данные 54</td>
-            <td>Данные 55</td>
-            <td>Данные 56</td>
-          </tr>
-          <tr>
-            <td>Данные 61</td>
-            <td>Данные 62</td>
-            <td>Данные 63</td>
-            <td>Данные 64</td>
-            <td>Данные 65</td>
-            <td>Данные 66</td>
-          </tr>
-          <tr>
-            <td>Данные 71</td>
-            <td>Данные 72</td>
-            <td>Данные 73</td>
-            <td>Данные 74</td>
-            <td>Данные 75</td>
-            <td>Данные 76</td>
-          </tr>
-          <tr>
-            <td>Данные 81</td>
-            <td>Данные 82</td>
-            <td>Данные 83</td>
-            <td>Данные 84</td>
-            <td>Данные 85</td>
-            <td>Данные 86</td>
-          </tr>
-        </tbody>
-      </table>
+            <tr>
+              <td><span>Данные 21</span></td>
+              <td><span>Данные 22</span></td>
+              <td><span>Данные 23</span></td>
+              <td><span>Данные 24</span></td>
+              <td><span>Данные 25</span></td>
+              <td><span>Данные 26</span></td>
+            </tr>
+            <tr>
+              <td>Данные 31</td>
+              <td>Данные 32</td>
+              <td>Данные 33</td>
+              <td>Данные 34</td>
+              <td>Данные 35</td>
+              <td>Данные 36</td>
+            </tr>
+            <tr>
+              <td>Данные 41</td>
+              <td>Данные 42</td>
+              <td>Данные 43</td>
+              <td>Данные 44</td>
+              <td>Данные 45</td>
+              <td>Данные 46</td>
+            </tr>
+            <tr>
+              <td>Данные 51</td>
+              <td>Данные 52</td>
+              <td>Данные 53</td>
+              <td>Данные 54</td>
+              <td>Данные 55</td>
+              <td>Данные 56</td>
+            </tr>
+            <tr>
+              <td>Данные 61</td>
+              <td>Данные 62</td>
+              <td>Данные 63</td>
+              <td>Данные 64</td>
+              <td>Данные 65</td>
+              <td>Данные 66</td>
+            </tr>
+            <tr>
+              <td>Данные 71</td>
+              <td>Данные 72</td>
+              <td>Данные 73</td>
+              <td>Данные 74</td>
+              <td>Данные 75</td>
+              <td>Данные 76</td>
+            </tr>
+            <tr>
+              <td>Данные 81</td>
+              <td>Данные 82</td>
+              <td>Данные 83</td>
+              <td>Данные 84</td>
+              <td>Данные 85</td>
+              <td>Данные 86</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
+      
     </>
   )
 }
