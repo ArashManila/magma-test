d5c959573c893ce57ba1ca5540decf4dcd9c2439
Igor Wilhelm <wilgelm-igor@yandex.ru>
2025-02-11 10:26:47 +0600

    Добавление новой ошибки из-за пустого объекта

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -4,7 +4,14 @@ import { Projects } from "../components/types/types"
 
 export const api={
   getProjects: async ():Promise<Projects>=>{
-    const response = await fetch('/projects.json').then(((res:Response):Promise<Projects>=>res.json()))
+    const response = await fetch('/projects.json')
+    .then(((res:Response):Promise<Projects>=>{
+      return res.json();
+    }))
+    .then((data:Projects):Projects=>{
+      if(data.length === 0) throw new Error("Projects not found, object is empty");
+      return data;
+    });
     return response
   }
 }
